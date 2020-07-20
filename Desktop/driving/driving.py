d125c6100c51d90e68912a2622fb893e20f0d34b
Cindy Lin <u588070544@gmail.com>
2020-07-19 22:42:33 -0700

    add US part

---
+++ Desktop/driving/driving.py
@@ -0,0 +1,13 @@
+country = input('Where are you from:')
+age = input('Please insert your age:')
+age = int(age)
+if country == 'Taiwan':
+	if age >= 18:
+		print('You can take a driving license')
+	else:
+		print('you cannot take a driving license')
+elif country == 'America':
+	if age >= 16:
+		print('You can take a driving license')
+	else:
+		print('you cannot take a driving license')	
